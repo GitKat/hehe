40cd3d09151ee7834c37855c83dd8eee6a25562c
GitKat <saif287430@gmail.com>
2017-05-28 16:28:08 +0300

    s
    s

--- js/main.js
+++ js/main.js
@@ -22,8 +22,9 @@ var app = {
     },
 
     initialize: function() {
+          var self = this;
         this.store = new MemoryStore();
-        var self = this;
+
          self.showAlert('Store Initialized', 'Info');
     });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
